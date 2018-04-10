2adaec2ff673df162c9369662ced3250a13a3bad
Salikh Salikhov <18199151+gataura@users.noreply.github.com>
2018-04-10 20:44:01 +0300

    Create mysql.sh

---
+++ web/mysql.sh
@@ -0,0 +1,5 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
